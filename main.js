09a5009f5079da684a5aee477171ddec5642180b
Khopipah <khopipahfebriyanti2202@gmaol.com>
2020-10-07 10:13:00 +0700

    update navbar

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 alert('Selamat datang di Web Saya...')
+
+//kita buat sebuah fungsi yang akan memunculkan modal
+const showModal = () => {
+
+    //kita ambil elemen yang akan ditampilkan
+    let modal = document.querySelector('.modalMenu')
+
+    //kita atur style dari elemen tersebut
+    //jadkan dia terlihat
+    modal.style.display = 'flex'
+}
+
+//kita buat fungsi untuk hide elemen
+const hideModal = () => {
+    //kita ambil elemen yang akan ditampilkan
+    let modal = document.querySelector('.modalMenu')
+
+    //kita atur style dari elemen tersebut
+    //jadikan dia terlihat
+    modal.style.display = 'none'
+}
